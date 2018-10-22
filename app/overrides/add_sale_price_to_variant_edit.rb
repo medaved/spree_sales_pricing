71bb1a20f35c3d28f360ef6a5f52b8aeb68517a4
kde <medavedik@yandex.ru>
2018-10-22 15:03:47 +0300

    Fix value.

--- app/overrides/add_sale_price_to_variant_edit.rb
+++ app/overrides/add_sale_price_to_variant_edit.rb
@@ -5,7 +5,7 @@ Deface::Override.new(virtual_path: 'spree/admin/variants/_form',
     <div data-hook='sale_price'>
       <%= f.label :sale_price, raw(Spree.t(:sale_price) ) %>
       <%= f.text_field :sale_price, value:
-        number_to_currency(variant.sale_price, unit: ''), class: 'form-control' %>
+        number_to_currency(@variant.sale_price, unit: ''), class: 'form-control' %>
       <%= f.error_message_on :sale_price %>
     </div>
   ")
