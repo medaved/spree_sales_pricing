a0b3402dcd649892498f363815ee2fdb98f9bb88
kde <medavedik@yandex.ru>
2018-10-25 01:10:00 +0300

    Lost method.

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,8 @@
 Spree::Product.class_eval do
   delegate :sale_price, :sale_price=, to: :master
+
+  def price_in(currency)
+    return orig_price_in(currency) unless sale_price.present?
+    Spree::Price.new(variant_id: self.id, amount: self.sale_price, currency: currency)
+  end
 end
