9ed41ac7507f1f8012a5bd489481e8e54c11cd63
kde <medavedik@yandex.ru>
2018-10-27 10:54:16 +0300

    Fix save formatting sale price.

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -5,4 +5,8 @@ Spree::Variant.class_eval do
     return orig_price_in(currency) unless sale_price.present?
     Spree::Price.new(variant_id: self.id, amount: self.sale_price, currency: currency)
   end
+
+  def sale_price=(sale_price)
+    super(sale_price.to_money)
+  end
 end
